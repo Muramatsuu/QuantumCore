e187d250bd6d1ca40ce37f29e9b2b745f75375a5
Matsu <124514952+Muramatsuu@users.noreply.github.com>
2025-08-02 15:40:33 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/quantumcore/main.go
@@ -0,0 +1,23 @@
+// cmd/quantumcore/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"quantumcore/internal/quantumcore"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := quantumcore.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
